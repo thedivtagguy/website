46e62ec2cab6c6536eb4f8697bfc8fcb0a229106
Aman Bhargava <amanbhargava2001@gmail.com>
2021-10-26 23:06:52 +0530

    update github pages

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,9 +3,6 @@
     console.table(stories);
 </script>
 <a href="/">Home</a> 
-<a href="/projects">Projects</a>  
-<a href="/blog">Blog</a>
-<a href="/resume">Resume</a> 
 <!-- Iterate and show all story headings -->
 <ul>
 {#each stories as story}

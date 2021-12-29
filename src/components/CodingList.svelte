d54b5da8d0f645a4e4cbe1bdadae64ef1e21cfa2
Aman Bhargava <amanbhargava2001@gmail.com>
2021-12-29 20:47:41 +0530

    Fix Code Picture

--- src/components/CodingList.svelte
+++ src/components/CodingList.svelte
@@ -71,7 +71,7 @@
     }
 
     #code {
-        background-image:  url("common/assets/resources/adventures.png");
+        background-image:  url("/common/assets/resources/adventures.png");
         background-repeat: no-repeat;
         background-size: 30%;
         background-position: 11.8rem;

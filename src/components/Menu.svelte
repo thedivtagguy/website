0d2bcc4e196d85841c48be037216c8aef5739b79
Aman Bhargava <amanbhargava2001@gmail.com>
2021-12-30 07:54:08 +0530

    Add Prefetch to Menu

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -39,12 +39,14 @@
               <path stroke-width="2px" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
+
         </div>
         <div class="lg:flex lg:flex-grow items-center {menuShow ? 'flex' : 'hidden'}">
           <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
             {#each links as link}
               <li>
-                <a
+                <a 
+                  sveltekit:prefetch 
                   href={link.link}
                   class="px-3 py-2 font-sans flex items-center text-sm uppercase font-bold leading-snug text-black hover:opacity-75"
                 >

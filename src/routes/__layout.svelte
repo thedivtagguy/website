bbf0f44c538d95099d7d5fdc0e377474a8b47c06
Aman Bhargava <amanbhargava2001@gmail.com>
2021-12-30 08:00:31 +0530

    Set router to True

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,8 @@
   import "$styles/app.css";
   import Header from "$components/Header.svelte";
   import Menu from "$components/Menu.svelte";
+  export const router = true;
+
 </script>
 
 <main class="max-w-5xl mx-auto px-6" id="content">

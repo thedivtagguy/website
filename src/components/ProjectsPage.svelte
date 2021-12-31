587ae33d1b062f64186fc0bf50b9c0f05ec27b0a
Aman Bhargava <amanbhargava2001@gmail.com>
2021-12-31 17:32:41 +0530

    Add Listings Page

--- src/components/ProjectsPage.svelte
+++ src/components/ProjectsPage.svelte
@@ -63,22 +63,17 @@ console.log(items)
 </script>
 
 <main>
-  <div id="toc">
     <div class="relative w-1/2 h-1/2">
-      <aside>
-        <img src="/common/assets/resources/toc.png" alt="hand" width="180" class="hand" />
-        <Toc headingSelector="main :where(h2)" --toc-desktop-margin="1em 0 0 0;" />
+      <aside id="photo">
+        <img src="/common/assets/resources/dump.png" alt="hand" width="180" class="hand" />
+        <!-- <Toc headingSelector="main :where(h2)" --toc-desktop-margin="1em 0 0 0;" title='' getHeadingIds = null/> -->
       </aside>
     </div>
-  </div>
   <section class="flex  pb-4 justify-between items-center mx-auto">
     <h1 class="text-6xl font-bold font-serif text-center py-4">Projects</h1>
-   
   </section>
- 
   <h2 class="text-4xl font-bold font-serif text-left border-b-2 border-black pb-2 mb-2">Data Storytelling</h2>
   <section id="data" class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3  gap-4 py-4">
-
     <!-- Projects where category includes data -->
     {#each refined as project}
       {#if project.category.includes("story")}
@@ -88,6 +83,12 @@ console.log(items)
       {/if}
     {/each}
   </section>
+  <div class="relative w-1/2 h-1/2">
+    <aside id="www">
+      <img src="/common/assets/resources/www.png" alt="hand" width="180" class="hand" />
+      <!-- <Toc headingSelector="main :where(h2)" --toc-desktop-margin="1em 0 0 0;" title='' getHeadingIds = null/> -->
+    </aside>
+  </div>
   <h2 class="text-4xl font-bold font-serif text-left border-b-2 border-black pb-2 mb-2">Web Development</h2>
   <section id="web" class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3  gap-4 py-4">
     <!-- Projects where category includes data -->
@@ -99,6 +100,11 @@ console.log(items)
       {/if}
     {/each}
   </section>
+  <div class="relative w-1/2 h-1/2">
+    <aside id="science">
+      <img src="/common/assets/resources/science.png" alt="hand" width="180" class="hand" />
+    </aside>
+  </div>
   <h2 class="text-4xl font-bold font-serif text-left border-b-2 border-black pb-2 mb-2">R Programming</h2>
   <section id="r" class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3  gap-4 py-4 mb-20">
     <!-- Projects where category includes data -->
@@ -113,15 +119,25 @@ console.log(items)
 </main>
 
 <style>
- aside {
+ #photo {
     position: absolute;
     left: -13.5rem;
-    top: 2rem;
+    top: 8rem;
     max-width: 200px;
   }
 
-  #toc {
-    position: fixed;
+  #science {
+    position: absolute;
+    left: -13.5rem;
+    top: 1rem;
+    max-width: 200px;
+  }
+
+  #www {
+    position: absolute;
+    left: -13.5rem;
+    top: 0rem;
+    max-width: 200px;
   }
 
 </style>

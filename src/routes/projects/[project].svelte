42d2f0f2d5d0ba0723a2c3e2454bd6fc20df4561
Aman Bhargava <amanbhargava2001@gmail.com>
2022-01-09 15:47:22 +0530

    Add External Links

--- src/routes/projects/[project].svelte
+++ src/routes/projects/[project].svelte
@@ -65,18 +65,7 @@
 </script>
 <script>
     export let post;
-	export let mycode = `
-# Create a function to print squares of numbers in sequence.
-new.function <- function(a) {
-   for(i in 1:a) {
-      b <- i^2
-      print(b)
-   }
-}
 
-# Call the function new.function supplying 6 as an argument.
-new.function(6)
-`;
 </script>
 
 
@@ -128,8 +117,19 @@ new.function(6)
 			</div>
 		</section>
 
-		<Prism language="html">
-			{mycode}
+		<Prism language="r">
+			{`
+				# Create a function to print squares of numbers in sequence.
+				new.function <- function(a) {
+				   for(i in 1:a) {
+					  b <- i^2
+					  print(b)
+				   }
+				}
+				
+				# Call the function new.function supplying 6 as an argument.
+				new.function(6)
+				`}
 		</Prism>
 
 		<section class="mx-auto">

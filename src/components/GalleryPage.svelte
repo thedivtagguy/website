81c98a32a1894bfa6f0114ed3c5d1c863d74c34d
Aman Bhargava <amanbhargava2001@gmail.com>
2022-01-12 19:44:36 +0530

    Add Gallery Filters closes #7

--- src/components/GalleryPage.svelte
+++ src/components/GalleryPage.svelte
@@ -36,14 +36,14 @@
       <h1 class="text-5xl lg:text-6xl mx-auto text-white font-bold font-serif text-center py-4">Gallery</h1>
     </section>
    
-    <section class="z-2 min-h-screen border-t-2 border-gray-300 py-4 bg-white">
-      <section class="py-4 grid grid-cols-6 gap-4 sticky top-0 z-10 bg-white px-4">
-        <div class="col-span-2">
-          <p class="text-[17px] leading-tight">
+    <section class="z-2 min-h-screen border-t-2 border-gray-300 py-6 bg-white">
+      <section class="py-4 grid grid-cols-1 lg:grid-cols-7 xl:grid-cols-7 sm:grid-cols-7 md:grid-cols-7 gap-4 lg:sticky sm:sticky md:sticky xl:sticky relative top-0 z-10 bg-white px-4">
+        <div class="w-full col-span-7 lg:col-span-3 md:col-span-3 sm:col-span-3 xl:col-span-3">
+          <p class="text-[15px] leading-tight">
             Some selected visual and motion works. I am an infrared and monochrome photographer and I also meddle with Blender from time to time.
           </p>
         </div>
-          <div class="flex col-span-4 w-full flex-row items-center justify-center">
+          <div class="lg:flex md:flex sm:flex xl:flex col-span-4 hidden w-full flex-row items-center justify-center">
               <div class="form-check w-full">
                 <input class="form-check-input appearance-none h-5 w-5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox"  value="monochrome" bind:checked={isChecked} bind:group={filterTerm} id="monochrome">
                 <label class="form-check-label inline-block font-sans  uppercase font-bold  text-lg text-gray-800" for="monochrome">

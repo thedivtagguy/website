d05a0e1c3cedffab66a6a2a0d453f2d0d15b7705
thedivtagguy <amanbhargava2001@gmail.com>
2023-01-15 11:25:13 +0530

    Fixes external blog links

--- src/components/BlogListing.svelte
+++ src/components/BlogListing.svelte
@@ -1,35 +1,35 @@
 <script>
-    import colors from "$data/thumbnail-colors.json";
+  import colors from "$data/thumbnail-colors.json";
   import Button from "./helpers/Button.svelte";
-  
-    export let slug;
-    export let heading;
-    export let description;
-    export let featured;
-    export let link;
-    export let fullLink;
-    export let external;
-    export let isExternal;
-    export let i;
-    export let image;
-    export let date;
-    export let keyword;
-    export let day;
-    export let month;
-    export let monthShort;
-    export let category;
-    // Choose first item from category
-    const DEFAULT_COLOR = {
-      light: "hsl(0, 0%, 80%)",
-      dark: "hsl(0, 0%, 44%)",
-      darker: "red"
-    };
-  
-    const lookupColor = (version) => {
-      const match = colors.find((d) => d.slug === slug);
-      return match ? match[version] : DEFAULT_COLOR;
-    };
-    const style = `
+
+  export let slug;
+  export let heading;
+  export let description;
+  export let featured;
+  export let link;
+  export let fullLink;
+  export let external;
+  export let isExternal;
+  export let i;
+  export let image;
+  export let date;
+  export let keyword;
+  export let day;
+  export let month;
+  export let monthShort;
+  export let category;
+  // Choose first item from category
+  const DEFAULT_COLOR = {
+    light: "hsl(0, 0%, 80%)",
+    dark: "hsl(0, 0%, 44%)",
+    darker: "red"
+  };
+
+  const lookupColor = (version) => {
+    const match = colors.find((d) => d.slug === slug);
+    return match ? match[version] : DEFAULT_COLOR;
+  };
+  const style = `
     --name: ${slug}
       --light: ${lookupColor("light")};
       --dark: ${lookupColor("dark")};
@@ -39,30 +39,25 @@
       --default-darker: ${DEFAULT_COLOR["darker"]};
     `;
   console.log(fullLink);
-  
-  export let url;
-  
-  if(isExternal == "TRUE" || isExternal == "true") {
-    url = fullLink;
-  
-  }
-  else if (category == "blog")
-  {
-    url = "/blog/" + link;
-  }
-  else {
-    url =  fullLink;
-  }
-  
-  </script>
 
+  export let url;
+
+  if (isExternal == "TRUE" || isExternal == "true") {
+    url = fullLink;
+  } else if (category == "blog") {
+    url = "/blog/" + link;
+  } else {
+    url = fullLink;
+  }
+</script>
 
 <!-- Display blogpost info -->
-<a href="{url}">
+<a href={url}  rel={external ? "external" : ''}>
   <!-- If i is even, add border. If odd, no border -->
-    <div class="w-full my-4 pb-8 group border-t-2 group border-black hover:cursor-pointer gap-4 flex flex-row justify-between items-center">
-      
-      {#if !image}
+  <div
+    class="w-full my-4 pb-8 group border-t-2 group border-black hover:cursor-pointer gap-4 flex flex-row justify-between items-center"
+  >
+    {#if !image}
       <img
         width="240"
         height="300"
@@ -73,34 +68,36 @@
                 /common/assets/thumbnails/640/{slug}.jpg 640w"
         sizes="(max-width: 320px) 640px, (max-width: 480px) 960px, 1280px"
       />
-      {:else}
-      <img
-        width="640"
-        height="500"
-        src="{image}"
-        alt="thumbnail for story"
-      />
-      {/if}
-          <div class="flex gap-2 flex-col w-full">
-            <h2 class="text-4xl w-full group-hover:underline decoration-wavy  flex-grow font-bold text-left">
-              {heading}
-            </h2>
+    {:else}
+      <img width="640" height="500" src={image} alt="thumbnail for story" />
+    {/if}
+    <div class="flex gap-2 flex-col w-full">
+      <h2
+        class="text-4xl w-full group-hover:underline decoration-wavy  flex-grow font-bold text-left"
+      >
+        {heading}
+      </h2>
 
-            <p class="text-left truncate text-ellipsis text- max-w-[400px] font-mono text-sm">
-              {description}
-            </p>
-          </div>
-          <!-- Display tags -->
-          <div class="flex min-w-[70px] flex-col gap-2">
-            {#each keyword as tag, i}
-            {#if i < 2}
-              <p class="text-sm capitalize text-center font-semibold text-white rounded-sm border-[3px] border-black bg-gray-700">{tag}</p>
-            {/if}
-            {/each}
-          </div>
-          <p class="font-bold mx-auto self-center rotate-90 font-serif text-center text-2xl  min-w-[100px] max-w-[150px] border-b-2 border- border-black">
-            {day} / {monthShort}
-         </p>
-
+      <p class="text-left truncate text-ellipsis text- max-w-[400px] font-mono text-sm">
+        {description}
+      </p>
     </div>
+    <!-- Display tags -->
+    <div class="flex min-w-[70px] flex-col gap-2">
+      {#each keyword as tag, i}
+        {#if i < 2}
+          <p
+            class="text-sm capitalize text-center font-semibold text-white rounded-sm border-[3px] border-black bg-gray-700"
+          >
+            {tag}
+          </p>
+        {/if}
+      {/each}
+    </div>
+    <p
+      class="font-bold mx-auto self-center rotate-90 font-serif text-center text-2xl  min-w-[100px] max-w-[150px] border-b-2 border- border-black"
+    >
+      {day} / {monthShort}
+    </p>
+  </div>
 </a>
